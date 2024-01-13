b352b295d70abb26b831ddf1328a3bd705d85428
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2024-01-14 02:39:07 +0530

    Spring Boot Demo

---
+++ 02-spring-boot-demo/src/test/java/com/umakantv/springboot/demo/hello_web/HelloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umakantv.springboot.demo.hello_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
